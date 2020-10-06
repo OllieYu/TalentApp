dee36e956c35c17cdc53ec4fac3ad59637b08ce9
Yingquan Yu <42467994+OllieYu@users.noreply.github.com>
2020-10-07 03:32:03 +1300

    Competition task

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -0,0 +1,68 @@
+﻿import React from 'react';
+import Cookies from 'js-cookie';
+import { Icon, Label, Button, Card, Popup } from 'semantic-ui-react';
+import moment from 'moment';
+
+export class JobSummaryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.selectJob = this.selectJob.bind(this)
+    }
+
+    selectJob(id) {
+        console.log(id)
+        var cookies = Cookies.get('talentAuthToken');
+        $.ajax({
+            url: 'http://localhost:51689/listing/listing/closeJob',
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            dataType: "json",
+            data: "5f79e5d39960b42b5ce5c1a5",
+            success: function (res) {
+
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status)
+            }
+        }) 
+    }
+
+    render() {
+        // console.log(this.props.id)
+        return(    
+        <Card style={{ minHeight: 350 }}>
+            <Card.Content>
+                <Card.Header>{this.props.title}</Card.Header>
+                <Label color='black' ribbon='right'>
+                    <Icon name = 'user' /> 0
+                </Label>
+                <Card.Meta>{this.props.location['city']},{this.props.location['country']}</Card.Meta>
+                <Card.Description>
+                {this.props.summar}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button size='mini' negative>Expired</Button>
+                <Button.Group size='mini' floated='right'>
+                    <Button
+                        onClick={() => {this.selectJob(this.props.id)}}
+                        basic color='blue'>
+                    <Icon name = 'ban'/>Close
+                    </Button>
+                    <Button basic color='blue'>
+                    <Icon name = 'edit outline'/>Edit
+                    </Button>
+                    <Button basic color='blue'>
+                    <Icon name = 'copy outline'/>Copy
+                    </Button>
+                </Button.Group>
+            </Card.Content>
+        </Card>
+        
+        
+        )
+    }
+}
---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -0,0 +1,145 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
+import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
+import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
+import { JobSummaryCard } from './JobSummaryCard.jsx';
+import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
+import { Header, Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, CardGroup } from 'semantic-ui-react';
+import _ from 'lodash'
+
+export default class ManageJob extends React.Component {
+    constructor(props) {
+        super(props);
+        let loader = loaderData
+        loader.allowedUsers.push("Employer");
+        loader.allowedUsers.push("Recruiter");
+        //console.log(loader)
+        this.state = {
+            loadJobs: [],
+            loaderData: loader,
+            activePage: 1,
+            sortBy: {
+                date: "desc"
+            },
+            filter: {
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true
+            },
+            totalPages: 1,
+            activeIndex: ""
+        }
+        this.loadData = this.loadData.bind(this);
+        this.init = this.init.bind(this);
+        this.loadNewData = this.loadNewData.bind(this);
+        //your functions go here
+        this.loadData()
+    };
+
+    init() {
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = false;
+        this.setState({ loaderData });//comment this
+
+        //set loaderData.isLoading to false after getting data
+        //this.loadData(() =>
+        //    this.setState({ loaderData })
+        //)
+        
+        //console.log(this.state.loaderData)
+    }
+
+    componentDidMount() {
+        this.init();
+    };
+
+    loadData(callback) {
+        var link = 'http://localhost:51689/listing/listing/getEmployerJobs';
+        var cookies = Cookies.get('talentAuthToken');
+       // your ajax call and other logic goes here
+       $.ajax({
+        url: link,
+        headers: {
+            'Authorization': 'Bearer ' + cookies,
+            'Content-Type': 'application/json'
+        },
+        type: "GET",
+        dataType: "json",
+        success: function (res) {
+            this.setState({ 
+                loadJobs : res
+            })
+        }.bind(this),
+        error: function (res) {
+            console.log(res.status)
+        }
+    }) 
+    }
+
+    loadNewData(data) {
+        var loader = this.state.loaderData;
+        loader.isLoading = true;
+        data[loaderData] = loader;
+        this.setState(data, () => {
+            this.loadData(() => {
+                loader.isLoading = false;
+                this.setState({
+                    loadData: loader
+                })
+            })
+        });
+    }
+    
+    render() {
+        // console.log(this.state.loadJobs.myJobs)
+        return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+               <div className ="ui container">
+               <Segment vertical>
+                    <Header as='h1'>List of Jobs</Header>   
+                    <Icon name = "filter"/>Filter:&nbsp;
+                    <Dropdown text='Choose filter' inline>
+                        <Dropdown.Menu>
+                        <Dropdown.Item text='condition1' />
+                        <Dropdown.Item text='condition2' />
+                        <Dropdown.Item text='condition3' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Icon name = "calendar alternate outline"/>Sort by date:&nbsp;
+                    <Dropdown text='Newest first' inline>
+                        <Dropdown.Menu>
+                        <Dropdown.Item text='condition1' />
+                        <Dropdown.Item text='condition2' />
+                        <Dropdown.Item text='condition3' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Segment>
+                <CardGroup itemsPerRow={3}>
+                {_.map(this.state.loadJobs.myJobs, ({ id, title, location, summary }) => (
+                    <JobSummaryCard 
+                        key={id}
+                        id={id}
+                        title={title}
+                        location={location}
+                        summary={summary}
+                    />
+                ))}    
+                </CardGroup>
+                <Segment basic textAlign="center">
+                <Pagination
+                    defaultActivePage={1}
+                    firstItem={{ content: <Icon name='angle double left' />, icon: true }}
+                    lastItem={{ content: <Icon name='angle double right' />, icon: true }}
+                    prevItem={{ content: <Icon name='angle left' />, icon: true }}
+                    nextItem={{ content: <Icon name='angle right' />, icon: true }}
+                    totalPages={3}
+                />
+                </Segment>
+               </div>
+            </BodyWrapper>
+        )
+    }
+}
